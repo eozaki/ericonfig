2ddc8a9a1d01f5574c0c7d8280683309e6b663b4
Erick Ozaki <erick@gaming-rig.Home>
2023-10-05 17:33:41 +0100

    Linux ready :+1:

--- environment_script.sh
+++ environment_script.sh
@@ -1,65 +1,64 @@
 #! /bin/bash
 
 function linuxPreparation {
-# 	# Getting started with updates
-# 	sudo apt update && sudo apt full-upgrade -y
-# 	sudo apt-get install fish git vim meld software-properties-common ruby ruby-dev synapse xdotool libinput-tools libpq-dev libgmp3-dev curl cowsay fortune dconf neovim silversearcher-ag gnupg2 libreadline6-dev -y
+	# Getting started with updates
+	sudo apt update && sudo apt full-upgrade -y
+	sudo apt-get install fish git vim meld software-properties-common ruby ruby-dev synapse xdotool libinput-tools libpq-dev libgmp3-dev curl cowsay fortune dconf neovim silversearcher-ag gnupg2 libreadline6-dev -y
 
-#  	# Copying font files and making them available
-#  	mkdir ~/.fonts
-#  	cp -r "$PWD"/*.ttf ~/.fonts
-#  	cp -r "$PWD"/*.otf ~/.fonts
-#  	fc-cache -f -v
-# 	
-# 	# Setting terminal theme
-#  	# Set correct font
-#  	dconf write /io/elementary/terminal/settings/font "'Monaco for Powerline 13'"
-# 	dconf write /io/elementary/terminal/settings/theme "'custom'"
-#  
-#  	# Configuring git
-#         rm -rf ~/.gitconfig
-#  	ln -s "$PWD"/.gitconfig ~/.gitconfig
-# 
-# 	# Install asdf
-# 	rm -rf ~/.asdf
-# 	git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
-#  
-#  	# Making fish the default shell
-#  	chsh -s /usr/bin/fish
-# 
-# 	# Linking pre-configured config filed for fish
-#  	rm -rf ~/.config/fish
-#  	ln -s "$PWD"/fish ~/.config/fish
-#  	rm -rf ~/.config/omf
-#  	ln -s "$PWD"/omf ~/.config/omf
-#  
-# 	# Install bullet-train theme
-# 	omf install https://github.com/kobanyan/bullet-train-fish-theme
-# 
-# 	# Install git plugin for omf
-# 	omf install https://github.com/jhillyerd/plugin-git
-# 
-#  	# Using vim config file and installing needed xclip (clipboard manager)
-#  	sudo apt install xclip -y
-#  	rm ~/.vimrc
-#  	ln -s "$PWD"/vimrc ~/.vimrc
-#  
-#  	# Configuring vim plugins
-#  	rm -rf .vim
-#  	mkdir .vim
-#  	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#  	ln -s ~/.vim ~/.config/nvim
-# 	rm -rf ~/.config/nvim/init.vim
-#  	ln -s ~/.vimrc ~/.config/nvim/init.vim
-# 	nvim +VundleInstall
-#  
-  	# installing tmux
-  	sudo apt install tmux tmuxinator
-#  
-#  	# configuring tmux
-#   rm -rf ~/.tmux.conf
-#  	ln -s "$PWD"/tmux.conf ~/.tmux.conf
-# 
+	# Copying font files and making them available
+	mkdir ~/.fonts
+	cp -r "$PWD"/*.ttf ~/.fonts
+	cp -r "$PWD"/*.otf ~/.fonts
+	fc-cache -f -v
+
+	# Setting terminal theme
+	# Set correct font
+	dconf write /io/elementary/terminal/settings/font "'Monaco for Powerline 13'"
+	dconf write /io/elementary/terminal/settings/theme "'custom'"
+
+	# Configuring git
+	rm -rf ~/.gitconfig
+	ln -s "$PWD"/.gitconfig ~/.gitconfig
+
+	# Install asdf
+	rm -rf ~/.asdf
+	git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
+
+	# Making fish the default shell
+	chsh -s /usr/bin/fish
+
+	# Linking pre-configured config filed for fish
+	rm -rf ~/.config/fish
+	ln -s "$PWD"/fish ~/.config/fish
+	rm -rf ~/.config/omf
+	ln -s "$PWD"/omf ~/.config/omf
+
+	# Install bullet-train theme
+	omf install https://github.com/kobanyan/bullet-train-fish-theme
+
+	# Install git plugin for omf
+	omf install https://github.com/jhillyerd/plugin-git
+
+	# Using vim config file and installing needed xclip (clipboard manager)
+	sudo apt install xclip -y
+	rm ~/.vimrc
+	ln -s "$PWD"/vimrc ~/.vimrc
+
+	# Configuring vim plugins
+	rm -rf .vim
+	mkdir .vim
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	ln -s ~/.vim ~/.config/nvim
+	rm -rf ~/.config/nvim/init.vim
+	ln -s ~/.vimrc ~/.config/nvim/init.vim
+	nvim +VundleInstall
+
+	# installing tmux
+	sudo apt install tmux tmuxinator
+
+	# configuring tmux
+	rm -rf ~/.tmux.conf
+	ln -s "$PWD"/tmux.conf ~/.tmux.conf
 }
 
 platform='unknown'
