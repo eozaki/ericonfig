5bc9b380303e3b5407d30e8cb38ebc27756dee35
eozaki <erick.ozaki@gmail.com>
2023-10-08 14:08:26 +0000

    fix: incorrect main branch download link

--- super_duper_script.sh
+++ super_duper_script.sh
@@ -1,6 +1,7 @@
-wget http://192.168.1.78:30000/eozaki/ericonfig.git
-unzip master.zip
-mv ericonfig-master .ericonfig
-cd .ericonfig
+wget http://192.168.1.78:30000/eozaki/ericonfig/archive/main.zip
+rm -rf ericonfig
+unzip main.zip
+rm main.zip
+cd ericonfig
 echo $PWD
 ./environment_script.sh
